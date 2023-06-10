b3e576c1b0407cee7ff16e900647e09f6b0ad4c3
Daniel Schur <dschur13@gmail.com>
2023-06-10 13:49:23 -0400

    Update test-get-data.py

--- test-get-data.py
+++ test-get-data.py
@@ -1,5 +1,5 @@
 from nyct_gtfs import NYCTFeed
-feed = NYCTFeed("11", api_key="tUVO9eM8wV2nQuwSsWIer23kedIlYHwD3PM7ZHqc")
+feed = NYCTFeed("A", api_key="tUVO9eM8wV2nQuwSsWIer23kedIlYHwD3PM7ZHqc")
 
 # Read all trip (train) information published to the BDFM feed 
 trains = feed.trips
