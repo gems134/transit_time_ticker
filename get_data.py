1e21943be259b3f261b710c30ea6d3f289be35d7
gems134 <gems134@gmail.com>
2023-06-10 12:17:40 -0400

    Added get_data.py

---
+++ get_data.py
@@ -0,0 +1,4 @@
+import requests
+my_headers = {"x-api-key": "tUVO9eM8wV2nQuwSsWIer23kedIlYHwD3PM7ZHqc"}
+response = requests.get("https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace", headers=my_headers)
+print(response.text)
